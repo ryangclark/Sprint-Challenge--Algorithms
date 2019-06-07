4da9b377ad31e2a81b1a9a4ae833a8ae9396cf99
Clark <rclark010@gmail.com>
2019-06-07 13:22:45 -0600

    Challenge Complete

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,9 +96,21 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        while self.can_move_right():
+            self.swap_item()
+            
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
 
+            # go back to beginning
+            while not self.compare_item() == None:
+                self.move_left()
+
+            self.swap_item()
+            self.move_right()
+                    
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
